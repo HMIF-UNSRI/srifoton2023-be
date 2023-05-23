8f5905f94189399c2b2326c5fc5b01a0ac93f1a1
imancrrzii <imancarrazi777@gmail.com>
2023-05-23 23:39:59 +0700

    Create layout for dashboard admin

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,52 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">Dashboard</span>
+          <i class="mdi mdi-home menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">User</span>
+          <i class="mdi mdi-account-multiple menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">Comp. Programming</span>
+          <i class="mdi mdi-brain menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">UI/UX Design</span>
+          <i class="mdi mdi-brush menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">Web Development</span>
+          <i class="mdi mdi-laptop-windows menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">Mobile Legends</span>
+          <i class="mdi mdi-gamepad-variant menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">Seminar</span>
+          <i class="mdi mdi-bullhorn menu-icon"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">
+          <span class="menu-title">Icons</span>
+          <i class="mdi mdi-contacts menu-icon"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashboardAdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard/admin', [DashboardAdminController::class, 'index']);
