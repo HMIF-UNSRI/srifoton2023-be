7d0ebcf163ccd4ab770c7e56c8065b584a536a0f
dzakyy04 <dzakylinggau@gmail.com>
2023-05-26 14:28:07 +0700

    Change all error page layout

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,41 +28,4 @@ class Handler extends ExceptionHandler
             //
         });
     }
-
-    public function render($request, Throwable $exception)
-    {
-        if ($exception instanceof \Illuminate\Validation\ValidationException) {
-            return response()->view('errors.400', ['exception' => $exception], 400);
-        }
-
-        if ($exception instanceof \Illuminate\Auth\Access\AuthorizationException) {
-            return response()->view('errors.403', [], 403);
-        }
-
-        if ($exception instanceof NotFoundHttpException) {
-            return response()->view('errors.404', [], 404);
-        }
-
-        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
-            return response()->view('errors.419', [], 419);
-        }
-
-        if ($exception instanceof \ErrorException || $exception instanceof \Throwable) {
-            if ($request->expectsJson()) {
-                return response()->json(['error' => 'Internal Server Error'], 500);
-            } else {
-                if (!auth()->check()) {
-                    return redirect()->guest(route('login'));
-                }
-                return response()->view('errors.500', [], 500);
-            }
-        }
-
-        if ($exception instanceof \Illuminate\Database\QueryException) {
-            return response()->view('errors.503', [], 503);
-        }
-
-
-        return parent::render($request, $exception);
-    }
 }
