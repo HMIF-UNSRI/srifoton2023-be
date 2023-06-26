c490e135151095e4632e9a9c2266f3a51747bae7
dzakyy04 <dzakylinggau@gmail.com>
2023-06-26 11:30:36 +0700

    Create competitive_programming table

---
+++ database/migrations/2023_06_26_111035_create_competitive_programmings_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('competitive_programmings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('team_name')->unique();
+            $table->string('email')->unique();
+            // Anggota 1
+            $table->string('name1');
+            $table->string('nim1');
+            $table->string('college1');
+            $table->string('phone_number1');
+            $table->string('instagram1');
+            $table->string('id_card1');
+            // Anggota 2
+            $table->string('name2')->nullable();
+            $table->string('nim2')->nullable();
+            $table->string('college2')->nullable();
+            $table->string('phone_number2')->nullable();
+            $table->string('instagram2')->nullable();
+            $table->string('id_card2')->nullable();
+            // Anggota 3
+            $table->string('name3')->nullable();
+            $table->string('nim3')->nullable();
+            $table->string('college3')->nullable();
+            $table->string('phone_number3')->nullable();
+            $table->string('instagram3')->nullable();
+            $table->string('id_card3')->nullable();
+            // Payment Method
+            $table->string('proof')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->boolean('isVerified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('competitive_programmings');
+    }
+};
