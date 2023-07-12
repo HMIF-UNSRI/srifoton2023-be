f23a1a1e4d03d5f8d7b86083475f9dff2f47fb1d
dzakyy04 <dzakylinggau@gmail.com>
2023-07-12 14:53:49 +0700

    Modify verify email user

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,12 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         VerifyEmail::toMailUsing(function ($notifable, $url) {
-            $spaUrl = env('FRONTEND_URL') . 'verify-email/?email_verify_url=' . $url;
 
             return (new MailMessage)
                 ->subject('Verify Email Address')
                 ->line('Click the button below to verify your email address')
-                ->action('Verify Email Address', $spaUrl);
+                ->action('Verify Email Address', $url);
         });
     }
 }
