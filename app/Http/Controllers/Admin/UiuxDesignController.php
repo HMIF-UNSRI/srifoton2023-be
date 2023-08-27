b9ea479ef32f2d207edec30d36cc6378e84ceb3a
dzakyy04 <dzakylinggau@gmail.com>
2023-08-27 13:49:10 +0700

    Fix Admin/UiuxDesignController class name

--- app/Http/Controllers/Admin/UiuxDesignController.php
+++ app/Http/Controllers/Admin/UiuxDesignController.php
@@ -4,80 +4,77 @@ namespace App\Http\Controllers\Admin;
 
 use ZipArchive;
 use App\Helper\Helper;
-use App\Models\WebDevelopment;
+use App\Models\UiuxDesign;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Response;
 
-class WebDevelopmentController extends Controller
+class UiuxDesignController extends Controller
 {
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $this->authorize('web_development');
+            $this->authorize('uiux_design');
             return $next($request);
         });
     }
 
     public function index()
     {
-        $webdevs = WebDevelopment::all();
-        return view('dashboard.competition.web_development.index', compact('webdevs'));
+        $uiuxs = UiuxDesign::all();
+        return view('dashboard.competition.uiux_design.index', compact('uiuxs'));
     }
 
     public function show($id)
     {
-        $webdev = WebDevelopment::findOrFail($id);
-        $members = ($webdev->id_card3) ? 3 : (($webdev->id_card2) ? 2 : 1);
-        return view('dashboard.competition.web_development.show', compact('webdev', 'members'));
-
+        $uiux = UiuxDesign::findOrFail($id);
+        $members = ($uiux->id_card3) ? 3 : (($uiux->id_card2) ? 2 : 1);
+        return view('dashboard.competition.uiux_design.show', compact('uiux', 'members'));
     }
 
     public function update($id)
     {
-        $webdev = WebDevelopment::findOrFail($id);
-        $webdev->update(['isVerified' => true]);
+        $uiux = UiuxDesign::findOrFail($id);
+        $uiux->update(['isVerified' => true]);
 
-        Helper::sendWhatsappGroupInvitationEmail($webdev->email, $webdev->name1, $webdev->teamName, 'Web Development', 'https://chat.whatsapp.com/Hwp4lBgCudvH5rkALiolES');
+        Helper::sendWhatsappGroupInvitationEmail($uiux->email, $uiux->name1, $uiux->teamName, 'UIUX Design', 'https://chat.whatsapp.com/KMfV1b9VpTODBOG0l7VdOZ');
 
-        return redirect()->route('competition.webdev')->with('success', 'Verification Successfull');
+        return redirect()->route('competition.uiux')->with('success', 'Verification Successfull');
     }
 
     public function delete($id)
     {
-        $webdev = WebDevelopment::findOrFail($id);
-        $members = ($webdev->id_card3) ? 3 : (($webdev->id_card2) ? 2 : 1);
+        $uiux = UiuxDesign::findOrFail($id);
+        $members = ($uiux->id_card3) ? 3 : (($uiux->id_card2) ? 2 : 1);
         for ($i = 1; $i <= $members; $i++) {
-            Storage::disk('public')->delete($webdev->{"id_card$i"});
+            Storage::disk('public')->delete($uiux->{"id_card$i"});
         }
-        Storage::disk('public')->delete($webdev->proof);
+        Storage::disk('public')->delete($uiux->proof);
 
-        $webdev->delete();
+        $uiux->delete();
 
-        return redirect()->route('competition.webdev')->with('success', 'Delete Successfull');
+        return redirect()->route('competition.uiux')->with('success', 'Delete Successfull');
     }
 
     public function downloadSubmission($id)
     {
-
-        $webdev = WebDevelopment::findOrFail($id);
-        $submission = $webdev->submission;
+        $uiux = UiuxDesign::findOrFail($id);
+        $submission = $uiux->submission;
         $appUrl = env('APP_URL');
         $submission = public_path(str_replace($appUrl, '', $submission));
 
         if (file_exists($submission)) {
-            return Response::download($submission);
+            return Response::download($submission); // Change 'submission_file.pdf' to the desired file name
         } else {
             return redirect()->back()->with('error', 'Submission file not found.');
         }
-
     }
 
     public function downloadAllSubmission()
     {
-        $folderPath = public_path('storage/submission/web-development');
-        $zipFileName = 'web-development-all-submission.zip';
+        $folderPath = public_path('storage/submission/uiux-design');
+        $zipFileName = 'uiux-all-submission.zip';
 
         if (!File::isDirectory($folderPath) || count(File::files($folderPath)) === 0) {
             return redirect()->back()->with('error', 'Submission file not found.');
@@ -97,7 +94,4 @@ class WebDevelopmentController extends Controller
 
         return Response::download(public_path($zipFileName))->deleteFileAfterSend(true);
     }
-
-
-
 }
