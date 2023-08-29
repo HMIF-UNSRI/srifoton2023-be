e119337971eca9a9783052b9cf500e7756406dc0
dzakyy04 <dzakylinggau@gmail.com>
2023-08-29 16:43:58 +0700

    Make global function to send whatsapp to finance group

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helper;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\WhatsappGroupInvitationEmail;
 
@@ -21,5 +23,21 @@ class Helper
         return response()->json([
             'message' => 'Email berhasil dikirim',
         ]);
+    }
+
+    public static function sendAlertToFinance($competitionName, $teamName, $paymentMethod, $link)
+    {
+        $client = new Client();
+        $options = [
+            'json' => [
+                'device_id' => env('DEVICE_ID'),
+                'group' => 'CP Srifoton 2022',
+                'message' => "Pemberitahuan!\n" .
+                    "Terdapat pendaftar baru di kompetisi *$competitionName* dengan nama tim *$teamName* dengan metode pembayaran *$paymentMethod*. Segera lakukan verifikasi pembayaran di link berikut: $link\n" .
+                    "Terima Kasih"
+            ]
+        ];
+        $request = new Request('POST', 'https://app.whacenter.com/api/sendGroup');
+        $client->sendAsync($request, $options)->wait();
     }
 }
